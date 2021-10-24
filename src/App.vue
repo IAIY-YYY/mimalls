d0e8d9013b8c71efd5ca4fd359b5c4ac4f2c9afc
zbt <863372133@qq.com>
2021-10-24 15:46:20 +0800

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 // import axios from 'axios'
-import jsonp from 'jsonp'
+// import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   },
   data () {
     return {
-      data:0
+      
     }
   },
   mounted () {
     
-    // axios.get(url).then(res=>{
-    //   console.log(res);
-    // })
-
-    //JSONP跨域 
-    let url = "/activity/servicetime"
-    jsonp(url,(err,res)=>{
-      let result = res;
-      this.data = result;
-    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view/>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+import NavFooter from '../components/NavFooter.vue'
+import NavHeader from '../components/NavHeader.vue'
+export default {
+    name:'home',
+    components: { 
+      NavHeader, 
+      NavFooter, 
+    },
+    
+}
+</script>
+
+<style scoped>
+
+</style>
